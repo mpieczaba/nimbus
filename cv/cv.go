258cd13747478273b47b91f9a14a03b7b8885e42
Mikołaj Pieczaba <mikolaj@pieczaba.me>
2021-03-18 22:09:32 +0100

    feat: Add detecting faces method

--- cv/cv.go
+++ cv/cv.go
@@ -2,10 +2,12 @@ package cv
 
 type CV struct {
 	PerceptualHash *PerceptualHash
+	FaceDetect     *FaceDetect
 }
 
 func New() *CV {
 	return &CV{
 		PerceptualHash: NewPerceptualHash(),
+		FaceDetect:     NewFaceDetect(),
 	}
 }
